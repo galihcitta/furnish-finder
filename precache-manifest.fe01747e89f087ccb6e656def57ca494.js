ad0238b629462f3aa80635a746da4dccb63db3d5
galihcitta <galih.citta@gmail.com>
2020-05-29 19:22:24 +0700

    Updates

---
+++ precache-manifest.fe01747e89f087ccb6e656def57ca494.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7fbb947bac3b8608a202148f93f1576",
+    "url": "/furnish-finder/index.html"
+  },
+  {
+    "revision": "c6f546154ad734f4014a",
+    "url": "/furnish-finder/static/css/main.3b7825b0.chunk.css"
+  },
+  {
+    "revision": "b975301304d9bbe952e7",
+    "url": "/furnish-finder/static/js/2.a2d36119.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/furnish-finder/static/js/2.a2d36119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f546154ad734f4014a",
+    "url": "/furnish-finder/static/js/main.015adc0d.chunk.js"
+  },
+  {
+    "revision": "5fb9a3524959b89f1475",
+    "url": "/furnish-finder/static/js/runtime-main.aa501d0a.js"
+  }
+]);
